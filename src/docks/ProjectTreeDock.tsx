deb7c2241808ab8fc2799925e4f62ef64f2b8aa2
avijitroydgp7 <136145082+avijitroydgp7@users.noreply.github.com>
2025-10-25 12:08:46 +0530

    update

--- src/docks/ProjectTreeDock.tsx
+++ src/docks/ProjectTreeDock.tsx
@@ -4,21 +4,19 @@ export const ProjectTreeDock: React.FC = () => {
     return (
         <div className="dock-item-content">
             <ul className="tree-view">
-                <li className="tree-item">
-                    <span className="material-icons">folder</span>
-                    <span>Project</span>
-                    <ul className="tree-view">
-                        <li className="tree-item"><span className="material-icons">folder_open</span> System</li>
-                        <li className="tree-item"><span className="material-icons">folder_open</span> Screen
-                            <ul className="tree-view">
-                                <li className="tree-item"><span className="material-icons">web_asset</span> B-1: (All Layers)</li>
-                            </ul>
-                        </li>
-                        <li className="tree-item"><span className="material-icons">label</span> Label/Tag</li>
-                        <li className="tree-item"><span className="material-icons">comment</span> Comment</li>
-                        <li className="tree-item"><span className="material-icons">alarm</span> Alarm</li>
-                    </ul>
-                </li>
+                <li className="tree-item"><span className="material-icons">folder_open</span> System</li>
+                <li className="tree-item"><span className="material-icons">info</span> Project Information</li>
+                <li className="tree-item"><span className="material-icons">label</span> Tag</li>
+                <li className="tree-item"><span className="material-icons">comment</span> Comment</li>
+                <li className="tree-item"><span className="material-icons">alarm</span> Alarm</li>
+                <li className="tree-item"><span className="material-icons">description</span> Logging</li>
+                <li className="tree-item"><span className="material-icons">receipt</span> Recipe</li>
+                <li className="tree-item"><span className="material-icons">code</span> Script</li>
+                <li className="tree-item"><span className="material-icons">sync</span> Device Data Transfer</li>
+                <li className="tree-item"><span className="material-icons">flash_on</span> Trigger Action</li>
+                <li className="tree-item"><span className="material-icons">schedule</span> Time Action</li>
+                <li className="tree-item"><span className="material-icons">image</span> Image</li>
+                <li className="tree-item"><span className="material-icons">movie</span> Animation</li>
             </ul>
         </div>
     );
--- src/docks/ScreenTreeDock.tsx
+++ src/docks/ScreenTreeDock.tsx
@@ -3,7 +3,23 @@ import React from 'react';
 export const ScreenTreeDock: React.FC = () => {
     return (
         <div className="dock-item-content">
-            <p>Screen tree view</p>
+            <ul className="tree-view">
+                <li className="tree-item">
+                    <span className="material-icons">folder</span>
+                    <span>Screen Design</span>
+                </li>
+                <li className="tree-item">
+                    <span className="material-icons">folder_open</span>
+                    <span>Screens</span>
+                    <ul className="tree-view">
+                        <li className="tree-item"><span className="material-icons">web</span> Base Screens</li>
+                        <li className="tree-item"><span className="material-icons">window</span> Window Screens</li>
+                        <li className="tree-item"><span className="material-icons">description</span> Template</li>
+                        <li className="tree-item"><span className="material-icons">tv</span> Screens</li>
+                        <li className="tree-item"><span className="material-icons">widgets</span> Widgets</li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     );
 };
